916feba7503b4e8834f7612d57942d1ee21afd57
Daniel <daniellopezlozano@gmail.com>
2023-05-19 13:45:11 +0200

    Portfolio primera versio final

---
+++ README.md
@@ -0,0 +1,22 @@
+# Portfolio personal Daniel López Lozano
+
+Este proyecto es un resumen de todas mis habilidades experiencia profesional y una muestra de mis proyectos realizados.
+
+## Uso
+
+Existen proyectos que funcionan con Javascript unicamente y solo necesitas un navegador.
+Otro proyectos tienen su  propia API y por lo tanto tendrás que tener un servidor con capacidad de scripting en PHP
+Y otra un poco más concretos son codigo en Python o Java que en general no tienen dependencias que sea necesario instalar nada
+
+## Contribución
+
+Sientente libre de contribuir o copiar este repositorio para tu uso personal ya que he cogido cosas para poder construirlo
+
+## Créditos
+
+Daniel López Lozano
+FREE HTML TEMPLATE LICENSE BY HTML Codex
+
+## Licencia
+
+GNU 3.0
---
+++ mail/contact.php
@@ -0,0 +1,20 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "daniellopezlozano@gmail.com"; // Change this email to your //
+$subject = "$m_subject:  $name";
+$body = "Has recibido un mensaje desde tu portfolio.\n\n"."Aqui los detalles:\n\nNombre: $name\n\n\nCorreo: $email\n\nAsunto: $m_subject\n\nMensaje: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
